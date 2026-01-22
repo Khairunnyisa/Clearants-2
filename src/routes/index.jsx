a937c7961ba7d3ac41d83b08e51a5569b8427f29
khairunnisa batubara <khairunnyisa@gmail.com>
2026-01-22 15:05:06 +0700

    fixing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,42 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { MainLanding } from "../landing-page/main-landing";
 import { MainArticle } from "../article-page/main-article";
 import { DetailArticle } from "../detail-article/main-detailarticle";
 import { Asoc } from "../asoc-pages/main-asoc";
 import { Faq } from "../faq-page/main-faq";
 import MainLayout from "../MainLayout";
-
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     element: <MainLayout />,
     children: [
-      {
-        path: "/",
-        element: <MainLanding />,
-      },
+      { path: "/", element: <MainLanding /> },
       {
         path: "/article",
         children: [
-          {
-            path: "",
-            element: <MainArticle />,
-          },
-          {
-            path: ":articleId",
-            element: <DetailArticle />,
-          },
+          { path: true, element: <MainArticle /> },
+          { path: ":articleId", element: <DetailArticle /> },
         ],
       },
-
-      {
-        path: "/asoc",
-        element: <Asoc />,
-      },
-
-      {
-        path: "/faq",
-        element: <Faq />,
-      },
+      { path: "/asoc", element: <Asoc /> },
+      { path: "/faq", element: <Faq /> },
     ],
   },
 ]);
